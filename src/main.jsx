31ddfafbc74fea035d58c7381f392582d686e73a
BoudiHamed <bodynody1@gmail.com>
2026-04-13 20:19:31 +0200

    removing some comments

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import "./index.css";
-// 1. إنشاء الكلاينت (المحرك)
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* 2. تغليف التطبيق بالبروفايدر */}
     <QueryClientProvider client={queryClient}>
       <App />
-      {/* اختيارياً: أداة المطورين لرؤية الكاش */}
+
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
